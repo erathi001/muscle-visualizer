4efb2b9fd1492683fdbc1b6aff41e5a4da9eb0af
eshan <erathi@uw.edu>
2025-01-06 20:31:15 -0800

    commit

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -9,6 +9,7 @@ interface BodyPartProps {
 
 function BodyPart(props:BodyPartProps) {
   const [isHovered, setIsHovered] = React.useState(false);
+  const colors: Map<string,string> = new Map([['traps','red'], ['delts','orange'],['chest','yellow'],['abs','#01E3EA'],['biceps','#75E702'],['forearms','#3A7003'],['thighs','#0010B6'],['calves','#7000B6']])
 
   return (
     <svg 
